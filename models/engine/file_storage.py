c20f1e553e9f40798dd8c57b34ce83f02f52fbbe
Akinwalee <obatulafuad@gmail.com>
2024-01-11 11:25:06 +0000

    Implementing the file storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""File storage modulle for the AirBnB project"""
+
+import json
+
+
+class FileStorage:
+    """Class that handles serialization of \
+            objects to json string and store in \
+            a file, and vice versa"""
+
+    __file_path = "storage.json"
+    __objects = {}
+
+    def all(self):
+        """Returns __object (the dictionary containing all \
+                objects in the file storage)"""
+
+        return (self.__objects)
+
+    def new(self, obj):
+        """Creates a new object obj in __objects \
+                with key <obj class>.id"""
+
+        obj.to_dict()
+        obj_dict = obj.__dict__
+        key = "{}.{}".format(obj_dict["__class__"], obj_dict["id"])
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes and saves __object to JSON file \
+                __file_path"""
+
+        with open("{}".format(self.__file_path), "w", encoding="utf-8") as f:
+            json.dump(self.__objects, f)
+
+    def reload(self):
+        """Desrializes JSON file __file_path to \
+                __objects"""
+
+        if len(self.__file_path) != 0:
+            with open("{}".format(self.__file_path), "r+", encoding="utf-8") as f:
+                self.__objects = json.load(f)
