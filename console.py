e9f4aa0d48bbc47b6e01bebeab2ec53c522785d8
Praise Aribisala <aribisalapraise12@gmail.com>
2024-01-12 14:55:22 +0100

    Task 7 almost completed.

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Defines te entry point of the command interpreter."""
 import cmd
+from models import base_model
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +21,78 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, line):
+        if len(line) == 0:
+            print("** class name missing **")
+        elif line != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            model = BaseModel()
+            model.save()
+            print(model.id)
+
+    def do_show(self, line):
+        model = BaseModel()
+        # Split the arguments into a list
+        line_list = line.split()
+
+        if line_list >= 2:
+            className = line_list[0]
+            classId = line_list[-1]
+
+            if len(className) == 0:
+                print("** class name missing **")
+            elif className != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(classId) == 0:
+                print("** instance id missing **")
+            elif classId != model.instances[id]:
+                print("** no instance found **")
+            else:
+                print(model.__str__())
+
+
+    def do_destroy(self, line):
+         model = BaseModel()
+        # Split the arguments into a list
+        line_list = line.split()
+
+        if line_list >= 2:
+            className = line_list[0]
+            classId = line_list[-1]
+
+            if len(className) == 0:
+                print("** class name missing **")
+            elif className != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(classId) == 0:
+                print("** instance id missing **")
+            elif classId != model.id:
+                print("** no instance found **")
+            else:
+                del className and classId
+                model.save()
+
+
+    def do_all(self, line):
+        all_list = []
+        if line != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            model = Basemodel()
+            all_list.append(model.__str__())
+            print(all_list)
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
