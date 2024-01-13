22339ede14dc64ba3bdffb44225908c7111d2e4a
Praise Aribisala <aribisalapraise12@gmail.com>
2024-01-13 07:30:30 +0100

    Edited the update function.

--- console.py
+++ console.py
@@ -23,6 +23,8 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it \
+                (to the JSON file) and prints the id"""
         if len(line) == 0:
             print("** class name missing **")
         elif line != "BaseModel":
@@ -33,6 +35,8 @@ class HBNBCommand(cmd.Cmd):
             print(model.id)
 
     def do_show(self, line):
+        """Prints the string representation of an \
+                instance based on the class name and id."""
         model = BaseModel()
         # Split the arguments into a list
         line_list = line.split()
@@ -54,6 +58,7 @@ class HBNBCommand(cmd.Cmd):
 
 
     def do_destroy(self, line):
+        """Deletes an instance based on the class name and id."""
          model = BaseModel()
         # Split the arguments into a list
         line_list = line.split()
@@ -76,6 +81,8 @@ class HBNBCommand(cmd.Cmd):
 
 
     def do_all(self, line):
+        """ Prints all string representation of all instances \
+                based or not on the class name."""
         all_list = []
         if line != "BaseModel":
             print("** class doesn't exist **")
@@ -85,6 +92,8 @@ class HBNBCommand(cmd.Cmd):
             print(all_list)
 
     def do_update(self, line):
+        """Updates an instance based on the class name and id by \
+                adding or updating attribute (save the change into the JSON file)."""
         model = BaseModel()
         # Split the arguments into a list
         line_list = line.split()
@@ -93,7 +102,23 @@ class HBNBCommand(cmd.Cmd):
             className = line_list[0]
             classId = line_list[1]
             attributeName = line_list[2]
-            attributeValue = line_list[3]e
+            attributeValue = line_list[3]
+
+            if len(className) == 0:
+                print("** class name missing **")
+            elif className != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(classId) == 0:
+                print("** instance id missing **")
+            elif classId != model.id:
+                print("** no instance found **")
+            elif len(attributeName) == 0:
+                print("** attribute name is missing **")
+            elif len(attributeValue) == 0:
+                print("** value missing **")
+            else:
+                model.to_dict()
+                model.save()
 
 
 
