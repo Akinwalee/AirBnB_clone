f7b34e621b388596326f4a413df1278791c79c17
Obatula Fuad <74328942+Akinwalee@users.noreply.github.com>
2024-01-16 12:40:32 +0100

    again importing what I don't need

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Defines te entry point of the command interpreter."""
 import cmd
 import os
+import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
