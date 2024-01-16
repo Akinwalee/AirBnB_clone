cebf44a79e5e14ca92f706752425165da566cae2
Obatula Fuad <74328942+Akinwalee@users.noreply.github.com>
2024-01-16 12:23:44 +0100

    hotter fix

--- console.py
+++ console.py
@@ -183,16 +183,16 @@ class HBNBCommand(cmd.Cmd):
     def handle_save(self, model, key):
         """handles the saving for update"""
         # store = storage.FileStorage
-        self.storage.__objects[key] = model
+        storage.__objects[key] = model
 
-        path = self.storage.__file_path
+        path = storage.__file_path
         if os.path.exists(path):
             with open("{}".format(path), "r", encoding="utf-8") as f:
                 current = json.load(f)
         else:
             current = {}
 
-        current.update(self.storage.__objects)
+        current.update(storage.__objects)
         with open("{}".format(path), "w", encoding="utf-8") as f:
             json.dump(current, f)
 
