47e12048c5919ed852f95433eff52e41735959c3
Obatula Fuad <74328942+Akinwalee@users.noreply.github.com>
2024-01-26 18:34:28 +0100

    trying to resolve circular import

--- models/engine/models_wrapper.py
+++ models/engine/models_wrapper.py
@@ -1,34 +1,35 @@
 # models_wrapper.py
 
 
+from ..base_model import BaseModel
+from ..user import User
+from ..state import State
+from ..city import City
+from ..amenity import Amenity
+from ..place import Place
+from ..review import Review
+
 def to_model(objs_dict):
     objects = {}
 
     for key, value in objs_dict.items():
         class_name = value["__class__"]
 
-        # Lazy loading: import the class only when needed
+        # Use the imported classes directly
         if class_name == "BaseModel":
-            from ..base_model import BaseModel
             model = BaseModel(value)
         elif class_name == "User":
-            from ..user import User
             model = User(value)
         elif class_name == "State":
-            from ..state import State
             model = State(value)
         elif class_name == "City":
-            from ..city import City
             model = City(value)
         elif class_name == "Amenity":
-            from ..amenity import Amenity
             model = Amenity(value)
         elif class_name == "Place":
-            from ..place import Place
             model = Place(value)
         elif class_name == "Review":
-            from ..review import Review
             model = Review(value)
         objects[key] = model
 
-    return (objects)
+    return objects
