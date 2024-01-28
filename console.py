b2ff13c1965abb42e97b0e829a335b4f959d9beb
Obatula Fuad <74328942+Akinwalee@users.noreply.github.com>
2024-01-28 21:59:06 +0100

    debug

--- console.py
+++ console.py
@@ -287,7 +287,7 @@ class HBNBCommand(cmd.Cmd):
         if class_name not in obj_dict.keys():
             # print("** class doesn't exist **")
             print(class_name)
-            print(len(class))
+            print(len(class_name))
         else:
             for key, value in obj_dict.items():
                 if key.startswith(class_name):
