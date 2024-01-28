9290c08d77b3201779f8bbf7fac304efee2bd31d
Obatula Fuad <74328942+Akinwalee@users.noreply.github.com>
2024-01-28 16:45:28 +0100

    Attempting to fix bug

--- console.py
+++ console.py
@@ -243,7 +243,7 @@ class HBNBCommand(cmd.Cmd):
 
         if method.startswith("show") or method.startswith("destroy"):
             match = re.search(r'"(.+)"', method)
-            if match != "" and not match.isispace():
+            if not isinstance(match, None):
                 id = match.group(1)
                 arg = class_name + " " + id
                 if method.startswith("show"):
@@ -252,7 +252,7 @@ class HBNBCommand(cmd.Cmd):
                     self.do_destroy(arg)
         if method.startswith("update"):
             match = re.search(r'\((.+)\)', method)
-            if match != "" and not match.isispace():
+            if not isinstance(match, None):
                 arg = match.group(1)
                 args = class_name + ", " + arg
                 self.do_update(args)
